76f840fa445c729d8622df1dde0195e2d051e61e
Christian Lück <christian@lueck.tv>
2013-01-19 00:25:13 +0100

    Use composer and improve error handling

--- example/client.php
+++ example/client.php
@@ -1,9 +1,11 @@
 <?php
 
-$loop = React\Loop\Factory::create();
+require_once __DIR__.'/../vendor/autoload.php';
 
-$factory = Resolver\Factory();
-$resolver = $factory->createCached($loop, '8.8.8.8');
+$loop = React\EventLoop\Factory::create();
+
+$factory = new React\Dns\Resolver\Factory();
+$resolver = $factory->createCached('8.8.8.8', $loop);
 
 $factory = new Datagram\Factory();
 
@@ -16,10 +18,16 @@ $factory->createClient($loop, $resolver, 'localhost', 1234)->then(function (Data
         echo 'received "' . $message . '" from ' . $server->getAddress() . PHP_EOL;
     });
 
+    $client->on('error', function($error, $server) {
+        echo 'error from ' . $server . PHP_EOL;
+    });
+
     $n = 0;
     $loop->addPeriodicTimer(2.0, function() use ($client, &$n) {
-        $client->send('tick' . $n);
+        $client->send('tick' . ++$n);
     });
+}, function($error) {
+    echo 'ERROR: ' . $error->getMessage() . PHP_EOL;
 });
 
 $loop->run();
--- example/server.php
+++ example/server.php
@@ -1,11 +1,13 @@
 <?php
 
-$loop = React\Loop\Factory::create();
+require_once __DIR__.'/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
 
 $factory = new Datagram\Factory();
 
 $factory->createServer($loop, 1234)->then(function (Datagram\Server $server) {
-    $server->on('message', function($data, $client) {
+    $server->on('message', function($message, $client) {
         $client->send('hello '.$client->getAddress().'! echo: '.$message);
 
         // $server->send() is not available here
