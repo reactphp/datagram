056ef2d422ba35df20e272d150d2451a132d810b
Christian Lück <christian@lueck.tv>
2013-04-15 10:09:54 +0200

    Merge (pseudo) SocketReadable into Socket

--- Datagram/Socket.php
+++ Datagram/Socket.php
@@ -5,11 +5,15 @@ namespace Datagram;
 use React\EventLoop\LoopInterface;
 use Evenement\EventEmitter;
 
+/** @event message */
+// interface similar to Stream
 class Socket extends EventEmitter
 {
     protected $loop;
     protected $socket;
     protected $address;
+
+    public $bufferSize = 65536;
 
     public function __construct(LoopInterface $loop, $socket, $address)
     {
@@ -39,6 +43,57 @@ class Socket extends EventEmitter
             $target = $this->address;
         }
         stream_socket_sendto($this->socket, $data, 0, $target);
+    }
+
+
+    public function pause()
+    {
+        $this->loop->removeReadStream($this->socket);
+    }
+
+    public function resume()
+    {
+        $this->loop->addReadStream($this->socket, array($this, 'onReceive'));
+    }
+
+    public function onReceive($message)
+    {
+        $data = stream_socket_recvfrom($this->socket, $this->bufferSize, 0, $peer);
+
+        if ($data === false) {
+            // receiving data failed => remote side rejected one of our packets
+            // due to the nature of UDP, there's no way to tell which one exactly
+            // $peer is not filled either
+
+            // emit error message and local socket
+            $this->emit('error', array(new \Exception('Invalid message'), $this));
+            return;
+        }
+
+        $this->emit('message', array($data, $this->sanitizeAddress($peer)));
+    }
+
+    public function close()
+    {
+        $this->pause();
+        fclose($this->socket);
+        $this->socket = false;
+    }
+
+    private function sanitizeAddress($address)
+    {
+        // doc comment suggests IPv6 address is not enclosed in square brackets?
+
+        $pos = strrpos(':', $address);
+        // this is an IPv6 address which includes colons but no square brackets
+        if ($pos !== false && substr($address, 0, 1) !== '[') {
+            if (strpos(':', $address) < $pos) {
+                $port = substr($address, $pos + 1);
+                $address = '[' . substr($address, 0, $pos) . ']:' . $port;
+            }
+
+        }
+        return $address;
     }
 
     public function __toString()
