ba9adaa638a52a3303d0ea050641c59bd9a5658c
Christian Lück <christian@lueck.tv>
2013-06-23 23:09:49 +0200

    Demo Socket::end() in example

--- example/client.php
+++ example/client.php
@@ -21,13 +21,21 @@ $factory->createClient('localhost', 1234)->then(function (Datagram\Socket $clien
     });
 
     $n = 0;
-    $loop->addPeriodicTimer(2.0, function() use ($client, &$n) {
+    $tid = $loop->addPeriodicTimer(2.0, function() use ($client, &$n) {
         $client->send('tick' . ++$n);
     });
 
     // read input from STDIN and forward everything to server
-    $loop->addReadStream(STDIN, function () use ($client) {
-        $client->send(trim(fgets(STDIN, 2000)));
+    $loop->addReadStream(STDIN, function () use ($client, $loop, $tid) {
+        $msg = fgets(STDIN, 2000);
+        if ($msg === false) {
+            // EOF => flush client and stop perodic sending and waiting for input
+            $client->end();
+            $loop->cancelTimer($tid);
+            $loop->removeReadStream(STDIN);
+        } else {
+            $client->send(trim($msg));
+        }
     });
 }, function($error) {
     echo 'ERROR: ' . $error->getMessage() . PHP_EOL;
