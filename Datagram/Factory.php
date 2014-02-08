3ea2375acaf9b44837387a54744d21290c41283b
Christian Lück <christian@lueck.tv>
2014-02-08 13:12:31 +0100

    Hostnames in socket address will now be resolved via DNS again

--- Datagram/Factory.php
+++ Datagram/Factory.php
@@ -24,7 +24,7 @@ class Factory
         $factory = $this;
         $loop = $this->loop;
 
-        return $this->resolve($address)->then(function ($address) use ($loop) {
+        return $this->resolveAddress($address)->then(function ($address) use ($loop) {
             $socket = stream_socket_client($address, $errno, $errstr);
             if (!$socket) {
                 throw new Exception('Unable to create client socket: ' . $errstr, $errno);
@@ -39,7 +39,7 @@ class Factory
         $factory = $this;
         $loop = $this->loop;
 
-        return $this->resolve($address)->then(function ($address) use ($loop) {
+        return $this->resolveAddress($address)->then(function ($address) use ($loop) {
             $socket = stream_socket_server($address, $errno, $errstr, STREAM_SERVER_BIND);
             if (!$socket) {
                 throw new Exception('Unable to create server socket: ' . $errstr, $errno);
@@ -49,12 +49,40 @@ class Factory
         });
     }
 
-    protected function resolve($address)
+    protected function resolveAddress($address)
     {
         if (strpos($address, '://') === false) {
             $address = 'udp://' . $address;
         }
         $parts = parse_url($address);
+
+        if (!$parts || !isset($parts['host'])) {
+            return When::resolve($address);
+        }
+
+        // remove square brackets for IPv6 addresses
+        $host = trim($parts['host'], '[]');
+
+        return $this->resolveHost($host)->then(function ($host) use ($parts) {
+            $address = $parts['scheme'] . '://';
+
+            if (isset($parts['port']) && strpos($host, ':') !== false) {
+                // enclose IPv6 address in square brackets if a port will be appended
+                $host = '[' . $host . ']';
+            }
+
+            $address .= $host;
+
+            if (isset($parts['port'])) {
+                $address .= ':' . $parts['port'];
+            }
+
+            return $address;
+        });
+    }
+
+    protected function resolveHost($host)
+    {
         // there's no need to resolve if the host is already given as an IP address
         if (false !== filter_var($host, FILTER_VALIDATE_IP)) {
             return When::resolve($host);
@@ -65,17 +93,8 @@ class Factory
         }
 
         if ($this->resolver === null) {
-            return When::reject(\Exception('No resolver given in order to get IP address for given hostname'));
+            return When::reject(new Exception('No resolver given in order to get IP address for given hostname'));
         }
         return $this->resolver->resolve($host);
-    }
-
-    public function createAddress($host, $port)
-    {
-        if (strpos($host, ':') !== false) {
-            // enclose IPv6 address in square brackets
-            $host = '[' . $host . ']';
-        }
-        return $host . ':' . $port;
     }
 }
